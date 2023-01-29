16e32d9e68ed4f4bf1488be87347911d861bdff9
Jill Reese <ajreese@gmail.com>
2023-01-28 23:39:46 -0500

    Initial commit for Weekly Exercises 03

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,12 +8,44 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+           if (vals == null ) return false;
+
+            var answer = false;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false) { answer = true; }
+            }
+            return answer;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            var answer = 0;
+            if (numbers == null) return false;
+           
+            List<int> odds = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                if (!(item % 2 == 0))
+                {
+                    odds.Add(item);
+                }
+            }
+
+            foreach (var item2 in odds)
+            {
+                answer += item2;
+            }
+
+            if (!(answer % 2 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
@@ -22,7 +55,10 @@ namespace ChallengesWithTestsMark8
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char value = val => IList<char>.First<char>;
+            char answer = value;
+
+            return answer;
         }
 
         public char GetLastLetterOfString(string val)
@@ -32,7 +68,13 @@ namespace ChallengesWithTestsMark8
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            decimal answer = 0.0m;
+
+            if (divisor != 0)
+            {
+                answer = dividend / divisor;
+            }
+            return answer;
         }
 
         public int LastMinusFirst(int[] nums)
@@ -42,12 +84,36 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            List<int> nums = new List<int>();
+
+            for (var i = 1 ; i <= 100; i++)
+            {
+                if (!(i % 2 == 0))
+                {
+                    nums.Add(i);
+                }
+            }
+            int count = nums.Count;
+
+            int[] myOdds = new int[count];
+
+            foreach (var item in nums)
+            {
+                for (var j = 0; j < count; j++)
+                {
+                    myOdds[j] = item;
+                }
+            }
+            return myOdds;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+           
+            for (var i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
