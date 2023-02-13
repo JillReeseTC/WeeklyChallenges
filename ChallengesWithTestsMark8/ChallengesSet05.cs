6fbdb43dba387f76cc818c86c07e23d6f91db97e
Jill Reese <ajreese@gmail.com>
2023-02-12 23:13:43 -0500

    Weekly Set 05 complete

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -74,61 +74,26 @@ namespace ChallengesWithTestsMark8
             if (words == null || words.Length == 0) { return ""; }
 
             var sentence = "";
-            for (var i = 0; i < words.Length;i++)
-            {
-                var set = false;
-              
-                if (words[i] == "Hello" && i == 0)
-                {
-                    sentence = "Hello, ";
-                    set = true;
-                }
-                //Get rid of white space
-                if (words[i].Contains(" "))
-                {
-                    char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-                    var myString = words[i];
-                    var newString = "";
-                    for (var q = 0; q < words[i].Length; q++)
-                    {
-                        for (var lett = 0; lett < letters.Length; lett++)
-                        {
-                            if (myString[q] == letters[lett])
-                            {
-                                newString = newString + myString[q];
-                            }
-                        }
-                    }
-                    //var myString = string.Concat(words[i].Where(c => !Char.IsWhiteSpace(c)));
-                    if (newString != "" && i != words.Length - 1)
-                    { 
-                        sentence = sentence + myString + " ";
-                        set = true;
-                    }  
-                    if (newString == "")
-                    {
-                        set = true;
-                    }
-                }
 
-                //Add a space after the word unless it's the last word or the sentence is blank.
-                if (words[i] != "" && set == false && i == words.Length - 1)
+            foreach (var word in words)
+            {
+                if (word.Trim().Length > 0)
                 {
-                    sentence = sentence + words[i];
-                }
-                else
-                {
-                    sentence = sentence + words[i] + " ";
-                }
-                // End the sentence with a period, if there is a sentence.
-                if (i == words.Length - 1 && sentence != "")
-                {
-                    sentence = sentence + ".";
+                    if (word == "Hello")
+                    {
+                        sentence = word.Trim() + ", ";
+                    }
+                    sentence += word.Trim() + " ";
                 }
             }
-            
+            //There weren't any words
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+            sentence = sentence.Trim();
+            sentence += ".";
             return sentence;
-
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
